a8968979202003cbf972f673adf6a92970d7b556
Alexandre Pecker <alexandrepeckersampaio@hotmail.com>
2024-02-24 11:32:33 -0300

    Criado componente loading e adicionado fonte utilizada no projeto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,8 @@ import { View,Text } from 'react-native'
 
 export default function Home(){
     return(
-        <View>
-            <Text>
-                React Native!
-            </Text>
+        <View className="flex-1 pt-8">
+            <Text className="text-white text-2xl font-heading">React Native!</Text>
         </View>
     )
-}   
+}
